84de0759c61e421e3a9fe7db7c0f1f80b96773a5
GitOnMyLvl <wolfgang.vinatzer@gmail.com>
2026-04-17 21:02:50 +0200

    feat: detect ?popout param and render PopoutApp

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import TopBar from './components/TopBar'
 import Canvas from './components/canvas/Canvas'
+import PopoutApp from './components/PopoutApp'
 
 export default function App() {
+  const params = new URLSearchParams(window.location.search)
+  const popoutType = params.get('popout')
+
+  if (popoutType) {
+    let config = {}
+    try { config = JSON.parse(decodeURIComponent(params.get('config') ?? '{}')) } catch (_) {}
+    return <PopoutApp type={popoutType} config={config} />
+  }
+
   return (
     <div className="flex flex-col" style={{ height: '100dvh', overflow: 'hidden' }}>
       <TopBar />
